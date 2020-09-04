19121ab137932b080a46087b189ef149d5d77d3e
Tomas Hurrell <hurrelltomas@gmail.com>
2020-09-03 21:34:09 -0300

    Updated coords to match Argentina locations

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO user (name, email, phone_number, longitude, latitude) VALUES ('Centro de donaciones Donarte','donarte@gmail.com',1131112345,37.78,-12.41);
-INSERT INTO user (name, email, phone_number, longitude, latitude) VALUES ('Fundación personas','fundacion_personas@gmail.com',1131167890,43.78,-12.41);
-INSERT INTO user (name, email, phone_number, longitude, latitude) VALUES ('Comedor Quilmes','comedor_quilmes@gmail.com',1131110293,17.78,-10.41);
+INSERT INTO user (name, email, phone_number, longitude, latitude) VALUES ('Centro de donaciones Donarte','donarte@gmail.com',1131112345,-58.3,-34.72);
+INSERT INTO user (name, email, phone_number, longitude, latitude) VALUES ('Fundación personas','fundacion_personas@gmail.com',1131167890,-58.334,-34.71);
+INSERT INTO user (name, email, phone_number, longitude, latitude) VALUES ('Comedor Quilmes','comedor_quilmes@gmail.com',1131110293,-58.298,-34.75);
