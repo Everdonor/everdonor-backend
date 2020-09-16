be817ce09ba7f0e2e9a08c0345a641ff58b3b9a0
Tobias <tobiascalvento@hotmail.com>
2020-09-16 15:59:09 -0300

    Added routes to modify and report

--- src/main/resources/travis_setup.sql
+++ src/main/resources/travis_setup.sql
@@ -10,5 +10,6 @@ CREATE TABLE user
     name          varchar(500) NOT NULL,
     password      varchar(255) NOT NULL,
     phone_number  int          NOT NULL,
+    reportQuantity int         NOT NULL,
     PRIMARY KEY (id)
 )
