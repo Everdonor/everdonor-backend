8e306f7c3dd53bdc68e87b7a6a2b27a3b2ec657d
Tomas Hurrell <hurrelltomas@gmail.com>
2020-09-15 21:27:49 -0300

    added script to create user table in travis vm

---
+++ src/main/resources/travis_setup.sql
@@ -0,0 +1,13 @@
+CREATE TABLE user (
+                        id bigint NOT NULL AUTO_INCREMENT,
+                        address varchar(255) DEFAULT NULL,
+                        donation_type varchar(255) DEFAULT NULL,
+                        email varchar(255) DEFAULT NULL,
+                        image text,
+                        latitude float NOT NULL,
+                        longitude float NOT NULL,
+                        name varchar(500) NOT NULL,
+                        password varchar(255) NOT NULL,
+                        phone_number int NOT NULL,
+                        PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
